6b0ceca3e4a6f8fb4dabfb8e8621e6f99e2b8a4f
Emjy <emjy.ahyong@gmail.com>
2026-07-10 09:44:57 +0400

    fix: SpringBootTest retiré du contexte in CI

--- src/test/java/com/titravay/TiTravayApplicationTests.java
+++ src/test/java/com/titravay/TiTravayApplicationTests.java
@@ -1,9 +1,7 @@
 package com.titravay;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TiTravayApplicationTests {
 
     @Test
